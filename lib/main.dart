9809ace547b48970a186f49dc5cede6c6131b129
KKframe <keawsung.fame@gmail.com>
2022-01-16 11:48:17 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pig Weight Calculator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,94 +22,233 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Pig(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Pig extends StatelessWidget {
+  Pig({Key? key}) : super(key: key);
+  final lengthController = TextEditingController();
+  final girthController = TextEditingController(); //ตั้งตัวแปรระดับคลาส
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'PIG WEIGHT ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'CALCULATOR',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.pink, fontSize: 30),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'assets/images/pig.png',
+                  height: 180.0,
+                ),
+              ),
+              Row(
+                children: [
+                  buildExpandedLength(),
+                  buildExpandedGirth(),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          //โค้ดที่จะทำงานเมื่อกดปุ่ม
+                          var l = lengthController.text;
+                          var g = girthController.text;
+                          double? length = double.tryParse(l);
+                          double? girth = double.tryParse(g);
+                          if(length==null || girth==null){
+                            showDialog(context: context,  barrierDismissible: false,builder: (BuildContext context){
+                              return AlertDialog(
+                                title: Text('ERROR'),
+                                content:Text('Invalid input'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            });
+                          }else{
+                            var weight = (girth/100)*(girth/100)*(length/100)*69.3;
+                            //print(weight)
+                            var price = 112.50 * weight;
+                            var minW = 0.97 * weight;
+                            var maxW = 1.03 * weight;
+                            var minPrice = 0.97*price;
+                            var maxPrice = 1.03*price;
+                            showDialog(context: context,  barrierDismissible: false,builder: (BuildContext context){
+                              return AlertDialog(
+                                title: Row(
+                                  children: [
+                                    Image.asset('assets/images/ic_pig.png',height: 30,),
+                                    Text('RESULT'),
+                                  ],
+                                ),
+                                content:Text('Weight : ${minW.round()} - ${maxW.round()} kg \nPrice : ${minPrice.round()} - ${maxPrice.round()} Baht'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            });
+                          }
+                        },
+                        child: Text(
+                          'CALCULATE',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Expanded buildExpandedGirth() {
+    return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: const Offset(2.0, 5.0), //ทิศทางของเงา,
+                                blurRadius: 5.0,
+                                spreadRadius: 2.0)
+                          ]
+                        //border: Border.all(width: 5.0, color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'GIRTH',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              '(CM)',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top:6.0,bottom: 6.0),
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                controller: girthController,
+                                decoration: const InputDecoration(
+                                  hintText: 'Enter girth',
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                );
+  }
+
+  Expanded buildExpandedLength() {
+
+    return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: const Offset(2.0, 5.0), //ทิศทางของเงา,
+                                blurRadius: 5.0,
+                                spreadRadius: 2.0)
+                          ]
+                        //border: Border.all(width: 5.0, color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'LENGTH',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              '(CM)',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 6),
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                controller: lengthController,
+                                decoration: const InputDecoration(
+                                  hintText: 'Enter weight',
+                                ),
+                                ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
   }
 }
